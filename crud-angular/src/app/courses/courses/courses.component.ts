b4c8b5642d9dd4b2d0eeaba1be8dd91134fa8d67
daviwillpereira <daviwillpereira@gmail.com>
2022-09-29 18:17:07 -0300

    first commit, angular material, table, spinner, toolbar, button, first service list courses mocked

---
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { Course } from '../model/course';
+import { ErrorDialogComponent } from './../../shared/components/error-dialog/error-dialog.component';
+import { CoursesService } from './../services/courses.service';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+export class CoursesComponent implements OnInit {
+
+  courses$: Observable<Course[]>;
+
+  displayedColumns = ['name', 'category'];
+
+  constructor(private coursesService: CoursesService,
+              public dialog: MatDialog) {
+    this.courses$ = this.coursesService.list()
+      .pipe(
+        catchError(error => {
+          this.onError('Could not load courses.')
+          return of([])
+        })
+      );
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
+  }
+
+  ngOnInit(): void { }
+
+}
