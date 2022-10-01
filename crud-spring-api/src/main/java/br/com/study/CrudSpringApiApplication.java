91f91aa29d7f362abf10c36eef7208f78f11b712
daviwillpereira <daviwillpereira@gmail.com>
2022-10-01 11:32:58 -0300

    initial api commit

---
+++ crud-spring-api/src/main/java/br/com/study/CrudSpringApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringApiApplication.class, args);
+	}
+
+}
