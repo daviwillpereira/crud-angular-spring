930d6b29760388443826ca089944ed349ef4b2c6
daviwillpereira <daviwillpereira@gmail.com>
2022-10-07 08:34:15 -0300

    courses api controller, service, repo, list courses, and angular proxy config cross origin

--- crud-spring-api/src/main/java/br/com/study/CrudSpringApiApplication.java
+++ crud-spring-api/src/main/java/br/com/study/CrudSpringApiApplication.java
@@ -1,7 +1,12 @@
 package br.com.study;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import br.com.study.model.Course;
+import br.com.study.repository.CourseRepository;
 
 @SpringBootApplication
 public class CrudSpringApiApplication {
@@ -10,4 +15,15 @@ public class CrudSpringApiApplication {
 		SpringApplication.run(CrudSpringApiApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner initDB(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+			
+			Course c = new Course();
+			c.setName("Angular with Spring");
+			c.setCategory("front-end");
+			courseRepository.save(c);
+		};
+	}
 }
---
+++ crud-spring-api/src/main/java/br/com/study/controller/CourseController.java
@@ -0,0 +1,25 @@
+package br.com.study.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.study.model.Course;
+import br.com.study.repository.CourseRepository;
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor
+public class CourseController {
+	  
+	private final CourseRepository courseRepository;
+	
+	@GetMapping
+	public List<Course> list() {
+		return courseRepository.findAll();
+	}
+	
+}
